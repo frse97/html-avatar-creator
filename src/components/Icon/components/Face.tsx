2806fd6c8ae2e3dafde20937daa54938ff366055
= <=>
2021-10-07 15:28:33 +0200

    Created face icon.

---
+++ src/components/Icon/components/Face.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react';
+import Icon from '../Icon';
+import { IFaceIcon } from '../Icon.types';
+import './Face.scss';
+
+const Face: React.FC<IFaceIcon> = (props) => (
+  <Icon {...props}>
+    <div className="face" />
+    <div className="neck" />
+    <div className="body" />
+  </Icon>
+);
+
+export default memo(Face);
