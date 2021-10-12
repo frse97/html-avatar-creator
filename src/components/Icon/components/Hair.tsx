3f4b8c68143a6b89b49bd426c13cb613e00a856b
= <=>
2021-10-12 13:46:15 +0200

    Added hair icon.

--- src/components/Icon/components/Hair.tsx
+++ src/components/Icon/components/Hair.tsx
@@ -1,10 +1,14 @@
-import React, { memo } from 'react';
-import Icon from '../Icon';
-import { IHairIcon } from '../Icon.types';
-import './Hair.scss';
+import React, { memo } from "react";
+import Icon from "../Icon";
+import { IHairIcon } from "../Icon.types";
+import "./Hair.scss";
 
 const Hair: React.FC<IHairIcon> = (props) => (
-  <Icon {...props}><div className="hair" /></Icon>
+  <Icon {...props}>
+    <div className="hair-face" />
+    <div className="hair-form1" />
+    <div className="hair-form2" />
+  </Icon>
 );
 
 export default memo(Hair);
