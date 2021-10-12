d6c7457035b642393fac085359d8d08cb47ff4b4
= <=>
2021-10-12 13:31:33 +0200

    Added accessory icon.

--- src/components/Icon/components/Accessory.tsx
+++ src/components/Icon/components/Accessory.tsx
@@ -5,8 +5,14 @@ import "./Accessory.scss";
 
 const Accessory: React.FC<IAccessoryIcon> = (props) => (
   <Icon {...props}>
-    <div className="earing1">
-      
+    <div className="glasses">
+      <div className="left-eye">
+        <div className="glow"/>
+      </div>
+      <div className="right-eye">
+        <div className="glow" />
+      </div>
+      <div className="line" />
     </div>
   </Icon>
 );
