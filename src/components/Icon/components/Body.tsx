a92b167f03293917b9f7508b5219f5d94bd1252e
= <=>
2021-10-12 12:54:39 +0200

    Added body icon.

--- src/components/Icon/components/Body.tsx
+++ src/components/Icon/components/Body.tsx
@@ -1,10 +1,17 @@
-import React, { memo } from 'react';
-import Icon from '../Icon';
-import { IBodyIcon } from '../Icon.types';
-import './Body.scss';
+import React, { memo } from "react";
+import Icon from "../Icon";
+import { IBodyIcon } from "../Icon.types";
+import "./Body.scss";
 
 const Body: React.FC<IBodyIcon> = (props) => (
-  <Icon {...props}><div className="body-shape" /></Icon>
+  <Icon {...props}>
+    <div className="body-head" />
+    <div className="body-leftarm" />
+    <div className="body-rightarm" />
+    <div className="body-main" />
+    <div className="body-leftfoot" />
+    <div className="body-rightfoot" />
+  </Icon>
 );
 
 export default memo(Body);
